95b449c32f1f23b7d0a4828bea1af30d4c9f0685
stonedhesus <ionescu_paul@outlook.com>
2022-06-13 15:48:27 +0200

    Commenced working on the custom look of the buttons.

---
+++ main/src/main/snake/Test.java
@@ -0,0 +1,17 @@
+package main.snake;
+
+import main.snake.components.Snake;
+
+import java.awt.event.KeyEvent;
+import java.util.Scanner;
+
+public class Test {
+
+    public static void main(String[] args){
+
+        Snake first_test = new Snake();
+
+
+        System.out.println(first_test.toString());
+    }
+}
